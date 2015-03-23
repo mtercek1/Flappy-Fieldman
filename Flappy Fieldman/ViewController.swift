ee9ae9f7712ef41614045be994abb0ddeafbb58c
Charlie Tercek <Smith@smiths-mbp-3.guest.hws.org>
2015-03-23 09:59:38 -0700

    Initial Commit

---
+++ Flappy Fieldman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flappy Fieldman
+//
+//  Created by Charlie Tercek on 3/23/15.
+//  Copyright (c) 2015 GirlCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
